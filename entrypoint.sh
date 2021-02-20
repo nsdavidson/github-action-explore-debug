dbf7fdf5cde379e11e8c59c0a0974519d10621f9
Nolan Davidson <nolan.davidson@gmail.com>
2021-02-19 21:32:45 -0500

    initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "Debug information:"
+
+IFS=$'\n'
+for v in `env`; do
+  PREFIX=$(echo $v | cut -d '_' -f1)
+  if [[ $PREFIX == 'GITHUB' ]] || [[ $PREFIX == 'INPUT' ]]; then
+    echo "$v";
+  fi
+
+  echo "---"
+  echo "Event:"
+  jq . $INPUT_JQ_SELECTOR $GITHUB_EVENT_PATH
+done
